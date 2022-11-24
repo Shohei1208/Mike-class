91f8877275fd3b9209e2417ce3995aaa34c27a41
Shohei1208 <118422808+Shohei1208@users.noreply.github.com>
2022-11-24 14:09:25 +0900

    Update module1-3.js

--- module1-3.js
+++ module1-3.js
@@ -1,6 +1,6 @@
 let Name = "Shohei"
 let Classes = ["JBE", "ECON", "OB1", "JLI"];
-let String = "Hello, my name is " + Name + " and I am taking ";
+let String = "Hello!, my name is " + Name + " and I am taking ";
 for (let Index in Classes) {
     if (Index < Classes.length - 1) {
         String += Classes[Index]+",";
