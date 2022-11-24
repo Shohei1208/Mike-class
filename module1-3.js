398a7c297fbd1e3ef2027892f9b26722128ffb73
Shohei1208 <118422808+Shohei1208@users.noreply.github.com>
2022-11-24 14:07:13 +0900

    Add files via upload

---
+++ module1-3.js
@@ -0,0 +1,12 @@
+let Name = "Shohei"
+let Classes = ["JBE", "ECON", "OB1", "JLI"];
+let String = "Hello, my name is " + Name + " and I am taking ";
+for (let Index in Classes) {
+    if (Index < Classes.length - 1) {
+        String += Classes[Index]+",";
+    } else{
+        String += "and" + Classes[Index]+"."
+        ;
+    }
+}
+console.log(String);
